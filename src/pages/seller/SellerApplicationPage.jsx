51f8a981cc172e30c1debb1d596610164df17641
Erdem <erdemsincer123@icloud.com>
2025-07-31 11:23:14 +0300

    Create SellerApplicationPage to render seller form

---
+++ src/pages/seller/SellerApplicationPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SellerApplicationForm from "@/components/seller/SellerApplicationForm";
+
+const SellerApplicationPage = () => {
+  return <SellerApplicationForm />;
+};
+
+export default SellerApplicationPage;
