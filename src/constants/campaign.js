c063d7b180cf8f5c881d28a0590868fc94380d28
Erdem <erdemsincer123@icloud.com>
2025-10-11 22:27:20 +0300

    chore: add campaign-related constants (scopes, types, etc.)

---
+++ src/constants/campaign.js
@@ -0,0 +1,7 @@
+export const CAMPAIGN_KINDS = [
+  "BasketAmountOff","BasketPercentOff","ItemAmountOff","ItemPercentOff","BuyXGetY",
+];
+
+export const CAMPAIGN_SCOPES = ["AllProducts","SpecificProducts","Categories"];
+export const CAMPAIGN_STATUSES = ["Draft","Active","Paused","Ended"];
+export const DEFAULT_CURRENCY = "TRY";
