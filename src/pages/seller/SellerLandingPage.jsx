2cfe67fb8f9cae9db89cbaf7295ebd60ccbcf371
Erdem <erdemsincer123@icloud.com>
2025-11-01 22:28:21 +0300

    Updated SellerLandingPage.jsx

--- src/pages/seller/SellerLandingPage.jsx
+++ src/pages/seller/SellerLandingPage.jsx
@@ -5,18 +5,29 @@ import StepsSection from "@/components/sellerLanding/StepsSection";
 import TestimonialsSection from "@/components/sellerLanding/TestimonialsSection";
 import CTASection from "@/components/sellerLanding/CTASection";
 import PricingSection from "@/components/sellerLanding/PricingSection";
+import ExportMadeEasySection from "@/components/sellerLanding/ExportMadeEasySection";
+import ExportVurgulayanSection from "@/components/sellerLanding/ExportVurgulayanSection";
+import GrowthOpportunitiesSection from "@/components/sellerLanding/GrowthOpportunitiesSection";
+import GlobalOpportunitiesSection from "@/components/sellerLanding/GlobalOpportunitiesSection";
+import HeroImpactSection from "@/components/sellerLanding/HeroImpactSection";
 import Footer from "@/components/sellerLanding/Footer";
+
 
 
 const SellerLandingPage = () => {
   return (
     <div className="bg-white">
       <SellerHeader />
+      <HeroImpactSection />
+      <ExportVurgulayanSection />
+      <GrowthOpportunitiesSection />
+      <GlobalOpportunitiesSection />
+      <ExportMadeEasySection />
       <HeroSection />
       <FeaturesSection />
       <StepsSection />
       <PricingSection /> {/* <-- BURAYA EKLENDİ */}
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
       <CTASection />
        <Footer />
     </div>
